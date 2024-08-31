a07b88e8b45237e290ad6c007a8b59c8666f7a2d
firpotas kodri <kadrimurilo1000@gmail.com>
2024-08-31 23:53:22 +0000

    dados dos niveis banco

---
+++ quiz/dadosniveis/dadosniveis.js
@@ -0,0 +1,214 @@
+export const perguntas = 
+{
+    quizzes: [
+
+    {   
+        title: "Fácil",
+        questões: [
+        {
+            pergunta: "Qual é a principal característica dos herpesvírus?",
+            respostas: [
+                "Causam infecções latentes",
+                "São transmitidos por água",
+                "Causam doenças respiratórias",
+                "São vírus de RNA"
+            ],
+            certa: "Causam infecções latentes"
+        },
+        {
+            pergunta: "Qual é o principal vetor de transmissão dos arbovírus?",
+            respostas: [
+                "Carrapatos",
+                "Mosquitos",
+                "Flebotomíneos",
+                "Roedores"
+            ],
+            certa: "Mosquitos"
+        },
+        {
+            pergunta: "Qual vírus é conhecido por causar herpes labial?",
+            respostas: [
+                "HSV-1",
+                "HSV-2",
+                "Varicela-zóster",
+                "Epstein-Barr"
+            ],
+            certa: "HSV-1"
+        },
+        {
+            pergunta: "Os enterovírus pertencem a qual família de vírus?",
+            respostas: [
+                "Picornaviridae",
+                "Herpesviridae",
+                "Paramyxoviridae",
+                "Flaviviridae"
+            ],
+            certa: "Picornaviridae"
+        },
+        {
+            pergunta: "Qual é a principal forma de transmissão dos enterovírus?",
+            respostas: [
+                "Contato direto com secreções respiratórias",
+                "Picadas de mosquitos",
+                "Ingestão de alimentos contaminados",
+                "Contato sexual"
+            ],
+            certa: "Ingestão de alimentos contaminados"
+        },
+        {
+            pergunta: "Qual é a principal doença causada pelo vírus varicela-zóster?",
+            respostas: [
+                "Catapora",
+                "Herpes genital",
+                "Mononucleose",
+                "Pneumonia"
+            ],
+            certa: "Catapora"
+        },
+        {
+            pergunta: "Qual vírus é transmitido por picadas de mosquitos e carrapatos?",
+            respostas: [
+                "Herpesvírus",
+                "Enterovírus",
+                "Arbovírus",
+                "Coronavirus"
+            ],
+            certa: "Arbovírus"
+        },
+        {
+            pergunta: "Qual é o material genético dos herpesvírus?",
+            respostas: [
+                "RNA",
+                "DNA",
+                "RNA e DNA",
+                "Proteínas"
+            ],
+            certa: "DNA"
+        },
+        {
+            pergunta: "Qual é uma das doenças graves causadas pelo VSR?",
+            respostas: [
+                "Meningite",
+                "Pneumonia",
+                "Catapora",
+                "Mononucleose"
+            ],
+            certa: "Pneumonia"
+        },
+        {
+            pergunta: "Qual vírus é conhecido por causar mononucleose?",
+            respostas: [
+                "HSV-1",
+                "Epstein-Barr",
+                "Citomegalovírus",
+                "Varicela-zóster"
+            ],
+            certa: "Epstein-Barr" 
+        }
+    ]
+},
+{
+    title: "Médio",
+    questões: [
+        {
+            pergunta: "Os herpesvírus são classificados como vírus de qual tipo de material genético?",
+            respostas: [
+                "RNA",
+                "DNA",
+                "RNA e DNA",
+                "Proteínas"
+            ],
+            certa: "DNA" 
+        },
+        {
+            pergunta: "Qual é a característica principal dos arbovírus?",
+            respostas: [
+                "São transmitidos por água",
+                "São transmitidos por artrópodes",
+                " São transmitidos por contato sexual",
+                "Causam doenças digestivas"
+            ],
+            certa: "São transmitidos por artrópodes" 
+        },
+        {
+            pergunta: "Qual é uma das principais causas de doenças respiratórias graves em bebês e crianças?",
+            respostas: [
+                "Vírus da gripe",
+                "Vírus da varicela",
+                "Vírus Sincicial Respiratório (VSR)",
+                "Epstein-Barr"
+            ],
+            certa: "Vírus Sincicial Respiratório (VSR)" 
+        },
+        {
+            pergunta: "Qual vírus da família Herpesviridae é conhecido por causar catapora e herpes-zóster?",
+            respostas: [
+                "HSV-1",
+                "Varicela-zóster",
+                "Epstein-Barr",
+                "Citomegalovírus"
+            ],
+            certa: "Varicela-zóster" 
+        },
+        {
+            pergunta: "Qual é o principal fator que aumenta a suscetibilidade às infecções por herpesvírus?",
+            respostas: [
+                "Exposição ao sol",
+                "Sistema imunológico comprometido",
+                "Alimentação inadequada",
+                "Falta de higiene"
+            ],
+            certa: "Sistema imunológico comprometido" 
+        },
+        {
+            pergunta: "Qual é uma característica dos enterovírus?",
+            respostas: [
+                "Causam apenas infecções respiratórias",
+                "São altamente contagiosos",
+                "São transmitidos apenas por picadas de insetos",
+                "Causam infecções somente no trato digestivo"
+            ],
+            certa: "São altamente contagiosos" 
+        },
+        {
+            pergunta: "Qual é o papel dos arbovírus na saúde pública?",
+            respostas: [
+                "Causam doenças digestivas leves",
+                "São responsáveis por surtos sazonais e epidemias",
+                "São transmitidos apenas por contato direto",
+                "Causam doenças apenas em animais domésticos"
+            ],
+            certa: "São responsáveis por surtos sazonais e epidemias" 
+        },
+        {
+            pergunta: "Qual dos seguintes vírus não causa infecções latentes?",
+            respostas: [
+                "Herpesvírus",
+                "Enterovírus",
+                "Arbovírus",
+                "Coronavirus"
+            ],
+            certa: "Enterovírus" 
+        },
+        {
+            pergunta: "Qual vírus é conhecido por causar paralisia flácida aguda?",
+            respostas: [
+                "Epstein-Barr",
+                "Herpesvírus",
+                "Enterovírus",
+                "Arbovírus"
+            ],
+            certa: "Enterovírus" 
+        }
+    ]
+},
+{
+    title: "Dificil",
+    questões: [
+        {
+            
+        }
+    ]
+}
+]
+}
